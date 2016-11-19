1e35e1898afae89bd9a4ac38a3189463391ca940
Salina Wu <salinawu@uchicago.edu>
2016-11-19 02:52:26 -0600

    added senator info

--- StopBannon/StopBannon/webscraper.rb
+++ StopBannon/StopBannon/webscraper.rb
@@ -18,27 +18,39 @@ def webscrap()
     parse_page = Nokogiri::HTML(open(url))
     state_list = parse_page.css("tbody")
     
-    state = ""
     reps = Hash.new
-    for i in 0..50
-        state = state_list.css("h2")[i+1].text
-        puts state_list[i].css("tr").length
+    (0..55).each do |i|
+        state = states[parse_page.css("h2")[i+1].text]
         if state_list[i].css("tr")
             state_list[i].css("tr").each do |s|
-                name = i.css("td")[1].text.gsub(/\s+/, "")
-                party = i.css("td")[2].text
-                phone = i.css("td")[4].text
+                name = s.css("td")[1].text.gsub(/\s+/, "")
+                party = s.css("td")[2].text
+                phone = s.css("td")[4].text
                 
-                if !/District/.match(name) and !/At-Large/.match(name)
-                    p = Politician.new(name, party, phone, state, 'Rep')
-                    reps[name] = p
-                    puts name
-                    puts state
-                end
+                p = Politician.new(name, party, phone, state, 'Rep')
+                reps[name] = p
+                puts name
+
             end
         end
     end
-
+    
+    senators_url = 'www.senate.gov/general/contact_information/senators_cfm.cfm'
+    senate_page = Nokogiri::HTML(open(url))
+    
+    for i in (0..300).step(3)
+        
+        name = senate_page.css("td.contenttext[align='left']")[0].text.split("\n")[0]
+        party = senate_page.css("td.contenttext[align='left']")[i].text.split("\n")[2].gsub(/[^0-9A-Za-z ]/, '').split(" ")[0]
+        state = senate_page.css("td.contenttext[align='left']")[i].text.split("\n")[2].gsub(/[^0-9A-Za-z ]/, '').split(" ")[1]
+        number = senate_page.css("td.contenttext[align='left']")[i+1].text
+        
+        p = Politician.new(name, party, phone, state, 'Sen')
+        reps[name] = p
+        puts name
+    end
+    
+    return reps
 end
 
 webscrap()
