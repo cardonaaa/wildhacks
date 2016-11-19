0644fd9cc69b8991add3f66be0aabddae21fa33b
Kyle Jablon <kylejablon1@dhcp-10-105-110-159.wireless.northwestern.private>
2016-11-19 12:52:36 -0600

    mobile front end

---
+++ StopBannon/StopBannon/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  StopBannon
+//
+//  Created by Kyle Jablon on 11/19/16.
+//  Copyright (c) 2016 Kyle Jablon. All rights reserved.
+//
+
+import Foundation
+
+class User{
+//    var peopleToContact = [String]()
+//    var name: String
+    
+    
+//    init(name: String){
+//        self.name = name
+//    }
+}
