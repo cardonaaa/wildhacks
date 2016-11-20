7df4e3bc72e3c177f05046f19c2616d1e3773e73
Kyle Jablon <kylejablon1@dhcp-10-105-110-159.wireless.northwestern.private>
2016-11-19 20:17:08 -0600

    updated

--- stopBannon2/stopBannon2/CSVParser.swift
+++ stopBannon2/stopBannon2/CSVParser.swift
@@ -1,9 +1,33 @@
 //
-//  CSVParser.swift
-//  stopBannon2
+//  csvparser.swift
+//  StopBannon
 //
-//  Created by Kyle Jablon on 11/19/16.
-//  Copyright © 2016 Kyle Jablon. All rights reserved.
+//  Created by Kathleen Lu on 11/19/16.
+//  Copyright (c) 2016 Kyle Jablon. All rights reserved.
 //
 
 import Foundation
+
+class CSVParser {
+    func parseCSV () -> [Politician]? {
+        let csvURL = NSURL(string: "politicians.csv")!
+        let delimiter = ","
+        var items:[Politician]?
+        
+        if let content = String(contentsOfURL: csvURL, encoding: NSUTF8StringEncoding) {
+            items = []
+            let lines:[String] = content.componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet()) as [String]
+            
+            for line in lines {
+                var values:[String] = []
+                values = line.componentsSeparatedByString(delimiter)
+                // firstname, lastname, phone, position, state, party
+                let item = Politician(firstname: values[1], lastname: values[0], phone: values[3], position: values[5], state: values[4], party: values[2])
+                println(item)
+                items?.append(item)
+            }
+        }
+        
+        return items
+    }
+}
