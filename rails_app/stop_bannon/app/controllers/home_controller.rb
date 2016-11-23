08452cdf63794498d60843422a9d04f7a626d6da
Salina Wu <salinawu@uchicago.edu>
2016-11-23 15:38:54 -0600

    cleaned up phone numbers

--- rails_app/stop_bannon/app/controllers/home_controller.rb
+++ rails_app/stop_bannon/app/controllers/home_controller.rb
@@ -13,6 +13,10 @@ class HomeController < ApplicationController
       else
         @states[FULL_STATES[p.state]] = [p]
       end
+
+      number = p.phone.gsub(/[^0-9]/, '')
+      a, b, c = number[0..2], number[3..5], number[6..-1]
+      p.phone = [a, b, c].join('-')
     end
     #TODO sort states
     #@states = @states.sort
